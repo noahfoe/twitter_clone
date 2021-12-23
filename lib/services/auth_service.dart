194458b4b138503752c0855a18a6439e8aef68b3
noahfoe <noahfoley6@gmail.com>
2021-12-23 16:05:55 -0600

    removes unused import

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:twitter_clone/models/user.dart';
 import 'package:twitter_clone/screens/auth/signin.dart';
